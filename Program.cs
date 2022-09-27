572f5a524866ecd344654a43f8bf1ef6bac38c64
swatibongu <78065014+swatibongu@users.noreply.github.com>
2022-09-27 18:41:35 +0530

    Employee check Rate.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProgram
+{
+    internal class UC3
+    {
+        public static void dbc()
+        {
+            int Part_Time = 1;
+            int Full_Time = 2;
+            int empHrs = 0;
+            int empWage = 0;
+            int EmpRate = 20;
+            Random rd = new Random();
+            int empCheck = rd.Next(0, 3);
+            if (empCheck == Part_Time)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == Full_Time)
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EmpRate;
+            Console.WriteLine("Employee wage is:" + empWage);
+
+        }
+    }
+}
+
